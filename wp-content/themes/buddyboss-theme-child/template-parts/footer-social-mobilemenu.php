df766812f1e71bb794dea091e3e21bbfec9d67d5
chavodbharat <chavodbharat@gmail.com>
2023-01-01 17:19:33 +0530

    fix custom css

--- wp-content/themes/buddyboss-theme-child/template-parts/footer-social-mobilemenu.php
+++ wp-content/themes/buddyboss-theme-child/template-parts/footer-social-mobilemenu.php
@@ -49,11 +49,29 @@ $current_component = !empty($bp->current_component)
                   <h5><?php _e('Activity','myludis'); ?></h5>
               </a>
           </div>
+          <!-- <div class="item"> -->
+                <!-- <a href="#!" class="<?php //echo $m_activecl; ?>" >
+                    <img src="<?php //echo get_stylesheet_directory_uri(); ?>/assets/images/svg/social/foot-icon-message.svg" alt="Message" />
+                    <h5><?php //_e('Message','myludis'); ?></h5>
+                </a> -->
+
+              <!-- <a href="#!" class="moreBtn">
+                <img src="<?php //echo get_stylesheet_directory_uri(); ?>/assets/images/svg/social/foot-icon-more-o.svg" alt="More" />
+                 <h5><?php //_e('More','myludis'); ?></h5>
+              </a> -->
+              <!-- <div class="moreDrop">
+                <ul>
+                    <li><a href="<?php //echo bp_core_get_user_domain($userID); ?>messages/compose/">Message Compose <i class="bb-icon-l bb-icon-brand-aperture"></i></a></li>
+                    <li><a href="<?php //echo bp_core_get_user_domain($userID); ?>messages/compose/" class="<?php //echo $for_activecl; ?>">Support <i class="bb-icon-l bb-icon-brand-apple"></i></a></li>
+                    <li><a href="<?php //echo bp_core_get_user_domain($userID); ?>messages/compose/">Notification <i class="bb-icon-l bb-icon-award"></i></a></li>
+                </ul>
+              </div> -->
+          <!-- </div> -->
           <div class="item">
-          <a href="<?php echo bp_core_get_user_domain($userID); ?>messages/compose/" class="<?php echo $m_activecl; ?>" >
-              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/svg/social/foot-icon-message.svg" alt="Message" />
-                  <h5><?php _e('Message','myludis'); ?></h5>
-              </a>
+            <a href="<?php echo bp_core_get_user_domain($userID); ?>messages/compose/" class="<?php echo $m_activecl; ?>" >
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/svg/social/foot-icon-message.svg" alt="Message" />
+                    <h5><?php _e('Message','myludis'); ?></h5>
+                </a>
           </div>
           <div class="item add_activity_post" id="whats-new-form_01">
               <a href="#!" class="logoTh" >
